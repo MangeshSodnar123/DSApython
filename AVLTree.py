e5bbcecbd5e357329e666d52d8c4d3b266beb628
Mangesh Sodnar <82178071+MangeshSodnar123@users.noreply.github.com>
2023-01-31 16:41:20 +0530

    Update AVLTree.py

--- AVLTree.py
+++ AVLTree.py
@@ -32,7 +32,7 @@ def levelOrderTraversal(rootNode):
     else:
         customList = []
         customList.append(rootNode)
-        while not(len(customList == 0)):
+        while not(len(customList)==0):
             root = customList.pop(0)
             print(root.data)
             if root.leftChild is not None:
@@ -63,57 +63,65 @@ def getHeight(rootNode):
     if not rootNode:
         return 0
     return rootNode.height
-    
-def rightRotate(disbalancedNode):
-    newRoot = disbalancedNode.leftChild
-    disbalancedNode.leftChild = disbalancedNode.leftChild.rightChild
-    newRoot.rightChild = disbalancedNode
-    
-    disbalancedNode.height = 1 + max(getHeight(disbalancedNode.leftChild), getHeight(disbalancedNode.rightChild))
-    
-    newRoot.height = 1 + max(getHeight(newRoot.leftChild), getHeight(newRoot.rightChild))
-    
-    return newRoot
-    
-def leftRotate(disbalancedNode):
-    newRoot = disbalancedNode.rightChild
-    disbalancedNode.rightChild = disbalancedNode.rightChild.leftChild
-    newRoot.leftChild = disbalancedNode
-    
-    disbalancedNode.height = 1 + max(getHeight(disbalancedNode.leftChild), getHeight(disbalancedNode.rightChild))
-    
-    newRoot.height = 1 + max(getHeight(newRoot.leftChild), getHeight(newRoot.rightChild))
 
+def rightRotate(disbalanceNode):
+    newRoot = disbalanceNode.leftChild
+    disbalanceNode.leftChild = disbalanceNode.leftChild.rightChild
+    newRoot.rightChild = disbalanceNode
+    disbalanceNode.height = 1 + max(getHeight(disbalanceNode.leftChild), getHeight(disbalanceNode.rightChild))
+    newRoot.height = 1 + max(getHeight(newRoot.leftChild), getHeight(newRoot.rightChild))
     return newRoot
+
+def leftRotate(disbalanceNode):
+    newRoot = disbalanceNode.rightChild
+    disbalanceNode.rightChild = disbalanceNode.rightChild.leftChild
+    newRoot.leftChild = disbalanceNode
+    disbalanceNode.height = 1 + max(getHeight(disbalanceNode.leftChild), getHeight(disbalanceNode.rightChild))
+    newRoot.height = 1 + max(getHeight(newRoot.leftChild), getHeight(newRoot.rightChild))
+    return newRoot
+
     
 def getBalance(rootNode):
     if not rootNode:
         return
-    return getHeight(rootNode.leftChilf) - getHeight(rootNode.rightChild)
+    return getHeight(rootNode.leftChild) - getHeight(rootNode.rightChild)
 
 def insertNode(rootNode,nodeValue):
     if not rootNode:
         return AVLNode(nodeValue)
     elif nodeValue < rootNode.data:
-        rootNode.leftChild = insertNode(rootNode.leftChild,nodeValue)
+        rootNode.leftChild = insertNode(rootNode.leftChild, nodeValue)
     else:
-        rootNode.rightChild = insertNode(rootNode.rightChild,nodeValue)
+        rootNode.rightChild = insertNode(rootNode.rightChild, nodeValue)
     
     rootNode.height = 1 + max(getHeight(rootNode.leftChild), getHeight(rootNode.rightChild))
-    
     balance = getBalance(rootNode)
     
     if balance > 1 and nodeValue < rootNode.leftChild.data:
         return rightRotate(rootNode)
-    elif balance > 1 and nodeValue > rootNode.leftChild.data:
+    
+    if balance > 1 and nodeValue > rootNode.leftChild.data:
         rootNode.leftChild = leftRotate(rootNode.leftChild)
         return rightRotate(rootNode)
-    elif balance < -1 and nodeValue > rootnode.rightChild.data:
+    
+    if balance < -1 and nodeValue > rootNode.rightChild.data:
         return leftRotate(rootNode)
-    elif balance < -1 and nodeValue < rootNode.rightChild.data:
-        rootNode.rightChild = rotateRight(rootNode.rightChild)
-        return rotateLeft(rootNode)
+    
+    if balance < -1 and nodeValue < rootNode.rightChild.data:
+        rootNode.rightChild = rightRotate(rootNode.rightChild)
+        return leftRotate(rootNode)
     return rootNode
 
+newAVL = AVLNode(30)
+newAVL = insertNode(newAVL, 25)
+newAVL = insertNode(newAVL, 35)
+newAVL = insertNode(newAVL, 20)
+newAVL = insertNode(newAVL, 15)
+newAVL = insertNode(newAVL, 5)
+newAVL = insertNode(newAVL, 10)
+newAVL = insertNode(newAVL, 50)
+newAVL = insertNode(newAVL, 60)
+newAVL = insertNode(newAVL, 70)
+newAVL = insertNode(newAVL, 65)
+preOrderTraversal(newAVL)
 
-avl = AVLNode(50)

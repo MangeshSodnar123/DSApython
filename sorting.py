6c0c4a703f6cd56fdb5b2106028cad87b89fd2f2
Mangesh Sodnar <82178071+MangeshSodnar123@users.noreply.github.com>
2023-02-04 15:59:30 +0530

    selection sort added

--- sorting.py
+++ sorting.py
@@ -13,6 +13,17 @@ def bubbleSort(custList,orderType):
                     
     return custList
 
+def selectionSort(lst):
+    for i in range(len(lst)):
+        minIndex = i
+        for j in range(i+1,len(lst)):
+            if lst[minIndex] > lst[j]:
+                minIndex = j
+    
+        lst[minIndex],lst[i] = lst[i],lst[minIndex]
+        
+    return lst
+
 
 lst = [4,5,3,2,7,9,1,6,8,0]
 print(bubbleSort(lst,"Descending"))

15de092a5565799098fbaafd3711185d5a4e20ed
Mangesh Sodnar <82178071+MangeshSodnar123@users.noreply.github.com>
2023-02-06 17:50:46 +0530

    addition of addEdge(vertex1,vertext2) method.

--- graph2.py
+++ graph2.py
@@ -9,14 +9,26 @@ class Graph:
         #Check if vertex is already present or not. It not add it in adjecency list.
         if vertex not in self.adjecencyList.keys():
             self.adjecencyList[vertex] = []
+            return True
+        return False
     #add method to print this list.
     def printGraph(self):
         #use for iterator and .keys() method.
         for vertex in self.adjecencyList.keys():
             print(vertex,":",self.adjecencyList[vertex])
             
+    #method to add edge with parameters:vertex1,vertext2->bool
+    def addEdge(self,vertex1,vertex2):
+        #First check if both vertices are present in our dictionary or not. if not return false. else add edge and return true.
+        if vertex1 in self.adjecencyList.keys() and vertex2 in self.adjecencyList.keys():
+            self.adjecencyList[vertex1].append(vertex2)
+            self.adjecencyList[vertex2].append(vertex1)
+            return True
+        return False
 graph = Graph()
 graph.addVertex('A')
+graph.addVertex('B')
+graph.addEdge('A','B')
 graph.printGraph()
         
         

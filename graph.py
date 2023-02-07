1844017e19131b5da2e50d95b91066c7dde43fd8
Mangesh Sodnar <82178071+MangeshSodnar123@users.noreply.github.com>
2023-02-07 15:00:14 +0530

    breadth first search

--- graph.py
+++ graph.py
@@ -1,21 +1,31 @@
-#graph
-#create graph class with the help of python dictionary.
 class Graph:
     def __init__(self,gDict=None):
-        if gDict is None:
+        if gDict==None:
             self.gDict = {}
-        else:
-            self.gDict = gDict
+        self.gDict = gDict
     
-    #create addEdge method in it.
     def addEdge(self,vertex,edge):
         self.gDict[vertex].append(edge)
-        
-#initialize the graph class with customDict
-myDict = {'a':['b','c'],'b':['a','e','d'],'c':['a','e'],'d':['b','e','f'],'e':['b','d','f'],'f':['d','e']}
+    
+    def bft(self,vertex):
+        #declare list for visited and initiate it with vertex
+        visited = [vertex]
+        #declare list for queue and initiate it with vertex.
+        queue = [vertex]
+        #while(queue):
+        while(queue):
+            #dequeue the element and print it
+            dVertex = queue.pop(0)
+            print(dVertex)
+            #check if all element from edge list of dVertex is visited.
+            for adjVertex in self.gDict[dVertex]:
+                if adjVertex not in visited:
+                    #if not then enqueue it in queue and
+                    queue.append(adjVertex)
+                    # add it to visited list.
+                    visited.append(adjVertex)
+                
+myDict = {'a':['b','c'],'b':['a','d','e'],'c':['a','e'],'d':['b','e','f'],'e':['b','c','d','f'],'f':['d','e']}
+
 graph = Graph(myDict)
-#print the dictionary.
-print(graph.gDict)
-print("-------------------------------------------")
-graph.addEdge('a','z')
-print(graph.gDict)
+graph.bft('a')

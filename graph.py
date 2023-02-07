d8f4bd5f4e8a366fa9ac3a6dffccd88380e87ec4
Mangesh Sodnar <82178071+MangeshSodnar123@users.noreply.github.com>
2023-02-07 15:26:21 +0530

    depth first traversal

--- graph.py
+++ graph.py
@@ -25,7 +25,26 @@ class Graph:
                     # add it to visited list.
                     visited.append(adjVertex)
                 
+    def dft(self,vertex):
+        #declare list for visited = [vertex]
+        visited = [vertex]
+        # declare list for stack = [vertex]
+        stack = [vertex]
+        #run loop till stack becomes empty
+        while stack:
+            #pop last element from stack and print it
+            popVertex = stack.pop()
+            print(popVertex)
+            #run a loop through all adjecent verteces of the vertex
+            for adjVertex in self.gDict[popVertex]:
+                #check if adject vertex present in visited list
+                if adjVertex not in visited:
+                    #if not then add it to visited list
+                    visited.append(adjVertex)
+                    #add it to the queue
+                    stack.append(adjVertex)
+    
 myDict = {'a':['b','c'],'b':['a','d','e'],'c':['a','e'],'d':['b','e','f'],'e':['b','c','d','f'],'f':['d','e']}
 
 graph = Graph(myDict)
-graph.bft('a')
+graph.dft('a')

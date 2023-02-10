0fbdac3b06e9ef4c00836eb7dc932bf4597b7ff8
Mangesh Sodnar <82178071+MangeshSodnar123@users.noreply.github.com>
2023-02-10 18:44:07 +0530

    Update dijkestra.py

--- dijkestra.py
+++ dijkestra.py
@@ -24,45 +24,35 @@ class Node:
         
 class Dijkestra:
     def __init__(self):
-        #create a list for heap
         self.heap = []
-    #calculate function(startVertex);
+
     def calculate(self,startVertex):
-        #push start vertex to heap
         startVertex.minDistance = 0
         heapq.heappush(self.heap,startVertex)
         #run a while loop till heap not become empty.
         while self.heap:
-            #curVertex = heap pop vertex
             curVertex = heapq.heappop(self.heap)
-            #check if curVertex is visited or not:
             if curVertex.visited:
-                #if yes 'continue'
                 continue
             #visit neighbours of curVertex:
             for edge in curVertex.neighbours:
-                #start = edge.startVertex
                 start = edge.startVertex
-                #end = edge.endVertex
                 end = edge.endVertex
                 newDistance = start.minDistance + edge.weight
                 if(newDistance < end.minDistance):
                     end.minDistance = newDistance
                     end.predecessor = start
-                    #heap.push(end)
                     heapq.heappush(self.heap,end)
-            #curVertex.visited = true
+
             curVertex.visited = True
             
-    #function to get shortest distance(endvertex):
     def getShortestPath(self,endVertex):
-        #curVertex = endVertex
+
         curVertex = endVertex
-        #print(minDistancd ov endVertex)
+
         print(f'the minimum distance is {endVertex.minDistance}')
-        #run a while loop to print path
+
         while (curVertex is not None):
-            #curVertex = curVertex.predesessor to go back 
             print(curVertex.name, end=" ")
             curVertex = curVertex.predecessor
 

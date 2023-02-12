c0a1ada51108a81f56732a78e5398793c209e57c
Mangesh Sodnar <82178071+MangeshSodnar123@users.noreply.github.com>
2023-02-12 16:52:42 +0530

    Update disjointSet.py

--- disjointSet.py
+++ disjointSet.py
@@ -2,7 +2,9 @@ def getParent(graph,node):
     if graph[node]<0:
         return node
     else:
-        getParent(graph,graph[node])
+        parent = getParent(graph,graph[node])
+        graph[node] = parent
+        return parent
 
 def union(graph,node1,node2):
     par1 = getParent(graph,node1)
@@ -22,3 +24,19 @@ def union(graph,node1,node2):
             else:
                 graph[par2] = graph[par2] + graph[par1]
                 graph[par1] = par2
+                
+n = 7
+graph = {}
+for i in range(n):
+    graph[i] = -1
+    
+ipt = [[0,1],[1,2],[2,3],[4,5]]
+
+for u,v in ipt:
+    graph[u] = v
+
+print(graph)
+print(getParent(graph,0))
+print('______________')
+union(graph,1,4)
+print(graph)

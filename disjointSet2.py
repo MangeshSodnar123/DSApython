8373be9a391545d445771b5149608e22d5acb6f1
Mangesh Sodnar <82178071+MangeshSodnar123@users.noreply.github.com>
2023-02-12 18:22:21 +0530

    Update disjointSet2.py

--- disjointSet2.py
+++ disjointSet2.py
@@ -22,16 +22,37 @@ class DisjointSet:
             return self.find(self.parent[vertex])
             
     #union method(node1,node2)
+    def union(self,vertex1,vertex2):
         # get parents of both nodes
+        par1 = self.find(vertex1)
+        par2 = self.find(vertex2)
         #check if they belong to same parent
+        if par1 == par2:
             #if yes then print msg saying they can't be joined.
-        #else:
+            print(f'{vertex1} and {vertex2} cannot be joined as both have {par1} as a common parent')
+        else:
             #check rank of both parents:
+            if self.rank[par1] > self.rank[par2]:
+                self.parent[par2] = par1
+                self.rank[par1] += 1
             #Meke parent with bigger rank the parent of both families
+            elif self.rank[par2] > self.rank[par1]:
             #make any parent a parent of both families if ranks are same
+                self.parent[par1] = par2
+                self.rank[par2] += 1
+            else:
+                self.parent[par2] = par1
+                self.rank[par1] += 1
             #increase rank of final parent by 1
             
             
-vertices = ['A','B','C','D','E','F']
+vertices = ['A','B','C','D','E','F','G']
 ds = DisjointSet(vertices)
 print(ds.find('A'))
+ds.union('A','B')
+ds.union('B','C')
+ds.union('C','D')
+ds.union('E','F')
+ds.union('B','D')
+
+print(ds.parent)

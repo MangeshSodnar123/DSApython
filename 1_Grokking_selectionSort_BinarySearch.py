06b0e130fb57474c2651fa4fa071229825cccd2e
Mangesh Sodnar <82178071+MangeshSodnar123@users.noreply.github.com>
2023-02-24 15:15:22 +0530

    Create 1_Grokking_selectionSort_BinarySearch.py

---
+++ 1_Grokking_selectionSort_BinarySearch.py
@@ -0,0 +1,35 @@
+def selectionSort(arr):
+    sortedArr = []
+    for i in range(len(arr)):
+        smallestInd = findSmallest(arr)
+        sortedArr.append(arr.pop(smallestInd))
+    return sortedArr
+        
+def findSmallest(arr):
+    smallest = arr[0]
+    smallestInd = 0
+    for i in range(len(arr)):
+        if(arr[i]<smallest):
+            smallest = arr[i]
+            smallestInd = i
+    return smallestInd
+    
+def binarySearch(arr,element):
+    #declare left, right
+    left = 0
+    right = len(arr)-1
+    #while left<=right
+    while left <= right:
+        mid=(left+right)//2
+        if(arr[mid]==element):
+            return mid
+        elif(arr[mid]>element):
+            right = mid-1
+        elif(arr[mid]<element):
+            left = mid + 1
+    return -1
+
+arr = [6,4,8,9,2,3,1]
+sortedArr = selectionSort(arr)
+print(sortedArr)
+print(binarySearch(sortedArr,88))

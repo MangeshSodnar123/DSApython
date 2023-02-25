df5b156a5f57fd7a73c2c8771cbf8647e7770864
Mangesh Sodnar <82178071+MangeshSodnar123@users.noreply.github.com>
2023-02-25 18:57:31 +0530

    Update 3_Grokking_graph_breadthFirstSearch.py

--- 3_Grokking_graph_breadthFirstSearch.py
+++ 3_Grokking_graph_breadthFirstSearch.py
@@ -1,31 +1,41 @@
 from collections import deque
 
+#make a graph using dictionary
+graph = {}
+graph['mangesh'] = ['raj','ramr','don']
+graph['don'] = ['king','ramr','pranjal']
+graph['raj'] = ['lucky']
+graph['ramr'] = ['lucky']
+graph['king'] = ['pranjal']
+graph['pranjal'] = ['ramr']
+graph['lucky'] = ['rubym']
+graph['rubym'] = ['pranjal']
+
+#define a function to find a mango seller
 def isMangoSeller(name):
     return name[-1] == 'm'
-
-graph = {}
-graph['you'] = ['alice','bob','claire']
-graph['bob'] = ['anuj','peggy']
-graph['alice'] = ['peggy']
-graph['claire'] = ['thom','jonny']
-graph['anuj'] = []
-graph['peggy'] = []
-graph['thom'] = []
-graph['jonny'] = []
-
-def breadthFirstSearch(graph,root):
+    
+#define search function
+def search(graph,root):
+    #make a queue and add root in the queue
     queue = deque()
-    queue += graph[root]
+    queue.append(root)
+    #make a list for visited
     visited = []
+    #run a loop till queue is not empty
     while(queue):
+        #person = dequeu element
         person = queue.popleft()
+        #check if person is visited or not
         if person not in visited:
+            #if not then check if he/she is mango seller
             if isMangoSeller(person):
-                return f'{person} is Mango Seller.'
+                return f'{person} is a mango seller.'
+            #else add person to the visited list
             else:
-                queue += graph[person]
                 visited.append(person)
+                #and add his neighbours to the queue
+                queue.extend(graph[person])
     return False
-
-root = 'you'
-print(breadthFirstSearch(graph,root))
+    
+print(search(graph,'mangesh'))

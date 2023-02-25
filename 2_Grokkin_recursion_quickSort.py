b1bf0d5a677e7142066fcc749091c11822bb036c
Mangesh Sodnar <82178071+MangeshSodnar123@users.noreply.github.com>
2023-02-25 16:43:32 +0530

    Update 2_Grokkin_recursion_quickSort.py

--- 2_Grokkin_recursion_quickSort.py
+++ 2_Grokkin_recursion_quickSort.py
@@ -1,85 +1,85 @@
-# def countDown(num):
+def countDown(num):
     
-#     print(num)
+    print(num)
     
-#     #base Condition.
-#     if(num<=0):
-#         return
-#     #Recursive condition.
-#     else:
-#         countDown(num-1)
+    #base Condition.
+    if(num<=0):
+        return
+    #Recursive condition.
+    else:
+        countDown(num-1)
         
-# countDown(20)
+countDown(20)
 
-# #Write out a code for sum function that gives sum of all elements in the array.
-# def sumOfArray(arr):
-#     #base Condition
-#     if len(arr)==1:
-#         return arr[0]
-#     #recursive condition
-#     return arr.pop(0) + sumOfArray(arr)
+#Write out a code for sum function that gives sum of all elements in the array.
+def sumOfArray(arr):
+    #base Condition
+    if len(arr)==1:
+        return arr[0]
+    #recursive condition
+    return arr.pop(0) + sumOfArray(arr)
     
-# print(sumOfArray([1,2,3,4]))
+print(sumOfArray([1,2,3,4]))
 
-# #Write a recursive function to count the number of items in a list
-# def numberOfItems(arr):
-#     #base condition
-#     if len(arr)==1:
-#         return 1
-#     #recursive condition
-#     arr.pop()
-#     return 1 + numberOfItems(arr)
+#Write a recursive function to count the number of items in a list
+def numberOfItems(arr):
+    #base condition
+    if len(arr)==1:
+        return 1
+    #recursive condition
+    arr.pop()
+    return 1 + numberOfItems(arr)
     
-# print(numberOfItems([1,2,3,4,4,53,2,6]))
+print(numberOfItems([1,2,3,4,4,53,2,6]))
 
-# # #binary search using the recursion
-# def binarySearchRec(arr,left,right,ele):
-#     #base condition
-#     if(left<=right):
-#         mid = (left+right)//2
-#         # print(mid)
-#         if(arr[mid]==ele):
-#             return mid
-#         #recursive condition
-#         elif(arr[mid]>ele):
-#             return binarySearchRec(arr,left,mid-1,ele)
-#         else:
-#             return binarySearchRec(arr,mid+1,right,ele)
+# #binary search using the recursion
+def binarySearchRec(arr,left,right,ele):
+    #base condition
+    if(left<=right):
+        mid = (left+right)//2
+        # print(mid)
+        if(arr[mid]==ele):
+            return mid
+        #recursive condition
+        elif(arr[mid]>ele):
+            return binarySearchRec(arr,left,mid-1,ele)
+        else:
+            return binarySearchRec(arr,mid+1,right,ele)
             
-#     else:
-#         #if element is not present in the arr.
-#         return -1
-#     #return -1
+    else:
+        #if element is not present in the arr.
+        return -1
+    #return -1
 
-# arr = [0,1,2,3,4,5,6,7,8,9,10]
-# left = 0
-# right = len(arr)-1
-# ele = 100
+arr = [0,1,2,3,4,5,6,7,8,9,10]
+left = 0
+right = len(arr)-1
+ele = 100
 
-# print(binarySearchRec(arr,left,right,ele))
-
-
-# #find the maximum number in the list
-# def findMax(arr):
-#     #base condition
-#     #recursive condition
-#     maxi = 
+print(binarySearchRec(arr,left,right,ele))
 
 
-# ##quickSort algorithm
-# def quickSort(arr):
-#     #base condition
-#     if(len(arr)<2): #[],[1]
-#         return arr
-#     else:
-#         pivot = arr[0]
-#         smallers = [i for i in arr[1:] if i<=pivot]
-#         greaters = [i for i in arr[1:] if i>pivot]
-#     #recursive condition
-#         return quickSort(smallers)+[pivot]+quickSort(greaters)
+#find the maximum number in the list
+def findMax(arr):
+    #base condition
+    #recursive condition
+    maxi = 
+
+
+##quickSort algorithm
+def quickSort(arr):
+    #base condition
+    if(len(arr)<2): #[],[1]
+        return arr
+    else:
+        pivot = arr[0]
+        smallers = [i for i in arr[1:] if i<=pivot]
+        greaters = [i for i in arr[1:] if i>pivot]
+    #recursive condition
+        return quickSort(smallers)+[pivot]+quickSort(greaters)
 
     
-# arr = [5,3,6,71,2,8,9,33,4,6,22,0,8]
-# arr1 = []
-# arr2 = [7]
-# print(quickSort(arr2))
+arr = [5,3,6,71,2,8,9,33,4,6,22,0,8]
+arr1 = []
+arr2 = [7]
+print(quickSort(arr2))
